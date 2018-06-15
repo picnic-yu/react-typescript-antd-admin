05d2de689b757d727d42a51e93cb36897c3be38e
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-06-15 16:18:27 +0800

    添加登陆页面

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // 页面
-import Home             from './page/home';
 import Login            from './page/login';
 import Layout            from './component/Layout';
 class App extends React.Component{
@@ -11,9 +10,9 @@ class App extends React.Component{
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route path="/home" component={ Home }/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/" component={ Layout }/>
+                    <Route path="/home" component={ Layout }/>
+                    <Route path="/login" component={ Login }/>
+                    <Route path="/" component={ Login }/>
                 </Switch>
             </BrowserRouter>
         )
@@ -21,8 +20,8 @@ class App extends React.Component{
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <App />,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
 
--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,9 +1,55 @@
 import * as React from 'react';
-class Login extends React.Component {
-    public render () {
-        return(
-            <div className="Login">Login</div>
-        )
+import { Form, Icon, Input, Button } from 'antd';
+import {FormComponentProps} from 'antd/lib/form/Form';
+import { Row, Col } from 'antd';
+import './index.scss'
+const FormItem = Form.Item;
+interface IAppProps {
+    form?: any,
+    history?:any
+}
+class NormalLoginForm extends React.Component <IAppProps & FormComponentProps>{
+    public handleSubmit = (e:any) => {
+        e.preventDefault();
+        this.props.form.validateFields((err:any, values:any) => {
+            if (values.userName === 'chen' && values.password === 'admin') {
+                this.props.history.push('/home'); 
+            } else {
+                alert('登陆失败');
+            }
+        });
+    }
+    public render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Row type="flex" justify="space-around" align="middle">
+                <Col span={4}>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <FormItem>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名chen" />
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码admin" />
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登陆
+                            </Button>
+                        </FormItem>
+                    </Form>
+                </Col>               
+            </Row>
+        );
     }
 }
+
+const Login = Form.create<IAppProps>()(NormalLoginForm);
 export default Login;
