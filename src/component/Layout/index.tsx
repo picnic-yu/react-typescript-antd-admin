23510e0babf60fe4b49aa7f005fbdca9874953f2
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-06-20 12:56:03 +0800

    修改左侧切换显示的bug

--- src/component/Layout/index.tsx
+++ src/component/Layout/index.tsx
@@ -20,11 +20,11 @@ class Main extends React.Component {
            
             return nodes.map((item:any, i:any) => {
                 if (item.child.length > 0) {
-                    const subMenuItem = menuProcess(item.child)
+                    const subMenuItem = menuProcess(item.child);
                     return (
                     <SubMenu
                         key={'sub'+item.key}
-                        title={<span><Icon type={item.icon} /><span className="nav-text">{item.name}</span></span>}
+                        title={<span><Icon type={item.icon} /><span >{item.name}</span></span>}
                     >
                         {
                             subMenuItem
@@ -33,11 +33,16 @@ class Main extends React.Component {
                     )
                 } else {
                     return (
-                    <Menu.Item key={'menu'+item.key}>
-                        {
-                            item.url ? <Link to={item.url}>{item.icon && <Icon type={item.icon} />}{item.name}</Link> : <span>{item.icon && <Icon type={item.icon} />}{item.name}</span>
-                        }
-                    </Menu.Item>
+                        <Menu.Item key={'menu'+item.key}>
+                            {
+                                item.url ? 
+                                <Link to={item.url}>
+                                    <Icon type={item.icon} />
+                                    <span>{item.name}</span> 
+                                </Link> : 
+                                <span>{<Icon type={item.icon} />}{item.name}</span>
+                            }
+                        </Menu.Item>
                     )
                 }
             });
@@ -59,7 +64,7 @@ class Main extends React.Component {
                     collapsed={this.state.collapsed}
                     onCollapse={this.onCollapse}>
                     <div className="logo"/>>
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                    <Menu theme = "dark" defaultSelectedKeys={['1']} mode="inline">
                         {
                             menu
                         }
