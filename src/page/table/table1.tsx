307432f5a9ebb97f738e0687d1c24f71bb425507
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-06-21 13:24:06 +0800

    添加表格案例

--- src/page/table/table1.tsx
+++ src/page/table/table1.tsx
@@ -1,9 +1,68 @@
 import * as React from 'react';
+import { Table, Icon } from 'antd';
+
+const columns = [{
+    dataIndex: 'name',
+    key: 'name',
+    render(text:any) :any{
+        return <a href="#">{text}</a>;
+    },
+    title: '姓名'
+}, {
+    dataIndex: 'age',
+    key: 'age',
+    title: '年龄'
+}, {
+    dataIndex: 'address',
+    key: 'address',
+    title: '住址'
+}, {
+    key: 'operation',
+    render(text:any, record:any):any {
+        return (
+            <span>
+                <a href="#">操作一{record.name}</a>
+                >
+                <a href="#">操作二</a>
+                
+                <a href="#" className="ant-dropdown-link">
+                更多 <Icon type="down" />
+                </a>
+            </span>
+        );
+    },
+    title: '操作'
+}];
+const data = [{
+    key: '1',
+    address: '西湖区湖底公园1号',
+    age: 32,
+    
+    name: '胡彦斌'
+    
+}, {
+    
+    address: '西湖区湖底公园1号',
+    age: 42,
+    key: '2',
+    name: '胡彦祖'
+    
+}, {
+    
+    address: '西湖区湖底公园1号',
+    age: 32,
+    key: '3',
+    name: '李大嘴'
+}];
+
 class Table1 extends React.Component {
-    public render () {
-        return(
-            <div className="home">Table1</div>
-        )
+    public render() {
+        return (
+            <div>
+                <Table columns={columns} dataSource={data}  />
+            </div>
+        );
     }
 }
+
 export default Table1;
